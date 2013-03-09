938c207ec2bac14807582eb98e4bf2bf1bc86683
Erez Mazor <erez@erez-laptop-2.local>
2013-03-10 00:47:04 +0200

    added short version of formatBase1024KMGTP

--- src/js/Rickshaw.Fixtures.Number.js
+++ src/js/Rickshaw.Fixtures.Number.js
@@ -22,3 +22,15 @@ Rickshaw.Fixtures.Number.formatBase1024KMGTP = function(y) {
     else if (abs_y === 0)           { return '' }
     else                        { return y }
 };
+
+Rickshaw.Fixtures.Number.formatBase1024KMGTPShort = function(y) {
+    abs_y = Math.abs(y);
+    if (abs_y >= 1125899906842624)  { return (y / 1125899906842624).toFixed(2) + "P" }
+    else if (abs_y >= 1099511627776){ return (y / 1099511627776).toFixed(2) + "T" }
+    else if (abs_y >= 1073741824)   { return (y / 1073741824).toFixed(2) + "G" }
+    else if (abs_y >= 1048576)      { return (y / 1048576).toFixed(2) + "M" }
+    else if (abs_y >= 1024)         { return (y / 1024).toFixed(2) + "K" }
+    else if (abs_y < 1 && y > 0)    { return y.toFixed(2) }
+    else if (abs_y === 0)           { return '' }
+    else                            { return y }
+};
